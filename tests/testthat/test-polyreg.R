71d3a322a9f751dae35fb54b61d7441296a17369
gestimaton <gestimaton@gmail.com>
2025-02-23 21:00:08 +0900

    Create README.Rmd and implement test().

--- tests/testthat/test-polyreg.R
+++ tests/testthat/test-polyreg.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("polyreg produced expected coefficients for bmt dataset", {
+  data(bmt)
+  result <- polyreg(nuisance.model = Event(time, cause)~age+tcell, exposure = 'platelet', cens.model = Event(time,cause==0)~+1, data = bmt, effect.measure1='RR', effect.measure2='RR', time.point=24, outcome.type='COMPETINGRISK')
+  tested_coefficient <- round(result$out_coefficient,digit=2)
+  expected_coefficient <- c(-2.81, 1.01, -1.32, -0.48, -2.65, 0.41, 0.63, 0.12)
+  expect_equal(expected_coefficient, tested_coefficient)
 })
+
+test_that("polyreg produced expected first row of covariance matrix for bmt dataset", {
+  data(bmt)
+  result <- polyreg(nuisance.model = Event(time, cause)~age+tcell, exposure = 'platelet', cens.model = Event(time,cause==0)~+1, data = bmt, effect.measure1='RR', effect.measure2='RR', time.point=24, outcome.type='COMPETINGRISK')
+  tested_cov <- round(result$out_cov[1,],digit=2)
+  expected_cov <- c(0.09, -0.03, 0.00, -0.02, 0.05, -0.02, 0.00, -0.01)
+  expect_equal(expected_cov, tested_cov)
+})
+
